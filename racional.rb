2fc5aa3b7831f6c7061db0be98b4434f35fd4692
Abraham <abrahamblg@hotmail.es>
2013-10-18 21:58:45 +0100

    Arreglado bug y comenzada la parte de los numeros racionales

--- racional.rb
+++ racional.rb
@@ -3,4 +3,16 @@
 require "./gcd.rb"
 
 class Fraccion
+	attr_reader :numerador, :denominador # getters y setters
+	def initialize (num,den) #Inicilizador, recibe el denominador y el numerador, los simplifica usando el mcd (gcd) y los guarda.
+		mcd = gcd(num,den) #calculamos el gcd
+		@numerador = num/mcd 
+		@denominador = den/mcd
+	end
+
+	def to_s #para imprimir una fraccion
+		(@denominador == 1 ? "#{@numerador}" : "#{@numerador}/#{@denominador}")
+	end
 end
+
+a = Fraccion.new(2,1)
